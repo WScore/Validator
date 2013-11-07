1edfe67307b7f22c96e6e3b4b03682460f4a41cb
Asao <kamei@workspot.jp>
2013-11-07 15:23:31 +0900

    add test suites to execute all tests on Validation.

---
+++ tests/WStest02/All_Tests.php
@@ -0,0 +1,18 @@
+<?php
+
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+
+class All_Tests
+{
+    public static function suite()
+    {
+        $suite = new \PHPUnit_Framework_TestSuite( 'all tests for WScore\'s Validation' );
+        $folder = __DIR__ . '/Validation/';
+        $suite->addTestFile( $folder . 'Message_Test.php' );
+        $suite->addTestFile( $folder . 'Rules_Test.php' );
+        $suite->addTestFile( $folder . 'Validate_Test.php' );
+
+        return $suite;
+    }
+}
