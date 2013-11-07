18a568a70f5c8fac1229751c85eb9bcb734b0adb
Asao <kamei@workspot.jp>
2013-11-07 17:28:12 +0900

    add two simple test.

--- tests/WStest02/Validation/Validation_Test.php
+++ tests/WStest02/Validation/Validation_Test.php
@@ -1,6 +1,7 @@
 <?php
 namespace WStest02\Validation;
 
+use WScore\Validation\Rules;
 use WScore\Validation\Validate;
 use WScore\Validation\Validation;
 use WScore\Validation\ValueTO;
@@ -13,11 +14,15 @@ class Validation_Test extends \PHPUnit_Framework_TestCase
     /** @var \WScore\Validation\Validation */
     var $validate;
 
+    /** @var  Rules */
+    var $rules;
+
     public function setUp()
     {
         $this->validate = new Validation(
             Validate::factory()
         );
+        $this->rules = new Rules();
     }
 
     // +----------------------------------------------------------------------+
@@ -27,5 +32,32 @@ class Validation_Test extends \PHPUnit_Framework_TestCase
     function basic_class_type()
     {
         $this->assertEquals( 'WScore\Validation\Validation', get_class( $this->validate ) );
+    }
+
+    /**
+     * @test
+     */
+    function find_a_single_value()
+    {
+        $rule = $this->rules;
+        $this->validate->source( array( 'test' => 'tested' ) );
+        $got = $this->validate->push( 'test', $rule('text') );
+
+        $this->assertEquals( 'tested', $got );
+        $this->assertEquals( 'tested', $this->validate->pop( 'test' ) );
+    }
+
+    /**
+     * @test
+     */
+    function find_a_single_array_of_value()
+    {
+        $rule = $this->rules;
+        $test = array( 'tested', 'more test' );
+        $this->validate->source( array( 'test' => $test ) );
+        $got = $this->validate->push( 'test', $rule('text') );
+
+        $this->assertEquals( $test, $got );
+        $this->assertEquals( $test, $this->validate->pop( 'test' ) );
     }
 }
