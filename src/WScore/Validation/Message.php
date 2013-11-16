6df99f878dd00eae63440f10afbff4050743265c
Asao <kamei@workspot.jp>
2013-11-16 13:57:10 +0900

    sets message based on filter as well as its parameter.

--- src/WScore/Validation/Message.php
+++ src/WScore/Validation/Message.php
@@ -15,6 +15,13 @@ class Message
             'choice'    => 'invalid choice',
             'sameAs'    => 'value not the same',
             'sameEmpty' => 'missing value to compare',
+            'matches' => [
+                'number' => 'only number',
+                'int' => 'not an integer',
+                'float' => 'not a float number',
+                'code' => 'only alpha-numeric code',
+                'mail' => 'not a valid mail address',
+            ],
         );
     }
 
@@ -30,10 +37,21 @@ class Message
         if( strpos( $method, '::filter_' ) !== false ) {
             $method = substr( $method, strpos( $method, '::filter_' )+9 );
         }
-        if( isset( $this->messages[ $method ] ) ) {
-            $message = $this->messages[ $method ];
-        } else{
+        if( !isset( $this->messages[ $method ] ) ) 
+        {
             $message = $this->messages[ 0 ];
+        }
+        elseif( is_array( $this->messages[ $method ] ) ) 
+        {
+            $parameter = $value->getType();
+            if( isset( $this->messages[ $method ][ $parameter ] ) ) {
+                $message = $this->messages[ $method ][ $parameter ];
+            } else {
+                $message = $this->messages[ 0 ];
+            }
+        }
+        else {
+            $message = $this->messages[ $method ];
         }
         $value->setMessage( $message );
     }
