0b1ffa39f4eb00ac39602117d407212e4f5cb193
Asao <kamei@workspot.jp>
2013-11-16 14:48:37 +0900

    update Lang.{en|ja}.php files to reflect new message structure.

--- src/WScore/Validation/Locale/Lang.en.php
+++ src/WScore/Validation/Locale/Lang.en.php
@@ -2,14 +2,17 @@
 namespace WScore\Validation\Locale;
 
 return array(
-    'filter' => array(
-        'encoding'  => 'invalid encoding',
-        'required'  => 'required item',
-        'sameAs'    => 'value not the same',
-        'sameEmpty' => 'missing value to compare',
-    ),
-    'type' => array(
-        'email' => 'invalid email',
-        'date'  => 'invalid date',
-    ),
+    0           => 'invalid input',
+    'encoding'  => 'invalid encoding',
+    'required'  => 'required item',
+    'choice'    => 'invalid choice',
+    'sameAs'    => 'value not the same',
+    'sameEmpty' => 'missing value to compare',
+    'matches'   => [
+        'number' => 'only numbers (0-9)',
+        'int'    => 'not an integer',
+        'float'  => 'not a floating number',
+        'code'   => 'only alpha-numeric characters',
+        'mail'   => 'not a valid mail address',
+    ],
 );
--- src/WScore/Validation/Locale/Lang.ja.php
+++ src/WScore/Validation/Locale/Lang.ja.php
@@ -2,14 +2,17 @@
 namespace WScore\Validation\Locale;
 
 return array(
-    'filter' => array(
-        'encoding'  => '不正な文字コードです',
-        'required'  => '必須項目です',
-        'sameAs'    => '入力内容が一致しません',
-        'sameEmpty' => '確認用の項目を入力ください',
-    ),
-    'type' => array(
-        'email' => 'メールアドレスが間違っています',
-        'date'  => '日付が間違っています',
-    ),
+    0           => '入力内容を確認して下さい',
+    'encoding'  => '不正な文字コードです',
+    'required'  => '必須項目です',
+    'choice'    => '選択できない値です',
+    'sameAs'    => '入力内容が一致しません',
+    'sameEmpty' => '確認用の項目を入力ください',
+    'matches'   => [
+        'number' => '数値のみです(0-9)',
+        'int'    => '整数を入力してください',
+        'float'  => '数値を入力してください',
+        'code'   => '記号（半角英数字）です',
+        'mail'   => 'メールアドレスを入力してください',
+    ],
 );
