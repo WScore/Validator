9975663c3a582d688b885dbefcb655683e65c233
Asao <kamei@workspot.jp>
2014-05-02 13:27:37 +0900

    delete outdated scripts for creating instances.

--- scripts/validate.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace WScore\Validation;
-
-return new Validate(
-    new \WScore\Validation\Filter(),
-    new \WScore\Validation\Message(),
-    new \WScore\Validation\Rules()
-);
--- scripts/validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace WScore\Validation;
-
-return new \WScore\Validation\Validation(
-    include( __DIR__ . '/validate.php'),
-    new \WScore\Validation\Rules()
-);
