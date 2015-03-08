c324954f23cb5dbd46effae996853b8774bfe29c
Asao <kamei@workspot.jp>
2015-03-08 11:15:08 +0900

    removed Validation class; use ValidationFactory::on instead.

--- tests/Validation_1_0/Validation_Test.php
+++ tests/Validation_1_0/Validation_Test.php
@@ -3,39 +3,30 @@ namespace tests\Validation_1_0;
 
 require_once(dirname(__DIR__).'/autoloader.php');
 
-use WScore\Validation\Factory;
 use WScore\Validation\Rules;
-use WScore\Validation\Validation;
+use WScore\Validation\ValidationFactory;
 
 class Validation_Test extends \PHPUnit_Framework_TestCase
 {
     function test0()
     {
-        $v = Validation::on( [ 'test' => 'tested' ] );
+        $factory = new ValidationFactory();
+        $v = $factory->on( [ 'test' => 'tested' ] );
         $this->assertEquals( 'WScore\Validation\Dio', get_class($v) );
 
         $is = $v->is( 'test', Rules::text() );
         $this->assertEquals( 'tested', $is );
     }
 
-    function test_useInstance()
-    {
-        $v = Factory::buildDio();
-        Validation::useInstance( $v );
-        $v2 = Validation::on( ['test'=>'tested'] );
-        $this->assertSame( $v, $v2 );
-        Validation::useInstance(null);
-    }
-
     function test_locale()
     {
-        Validation::locale('en');
-        $v = Validation::on([]);
+        $factory = new ValidationFactory();
+        $v = $factory->on([]);
         $v->is( 'test', Rules::text()->required() );
         $this->assertEquals( 'required item', $v->message('test') );
 
-        Validation::locale('ja');
-        $v = Validation::on([]);
+        $factory = new ValidationFactory('ja');
+        $v = $factory->on([]);
         $v->is( 'test', Rules::text()->required() );
         $this->assertEquals( '必須項目です', $v->message('test') );
     }
@@ -45,8 +36,8 @@ class Validation_Test extends \PHPUnit_Framework_TestCase
      */
     function filter_invalid_integer_input()
     {
-        Validation::locale('en');
-        $v = Validation::on([
+        $factory = new ValidationFactory();
+        $v = $factory->on([
             'int' => '100',
             'big' => '101',
             'bad' => '12345678901234567890123456789012345678901234567890',
