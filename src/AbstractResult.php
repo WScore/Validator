21932a741857892a112be94fc669df7dc0b23ea0
asaokamei <asao.kamei@gmail.com>
2019-03-26 08:16:05 +0900

    use AbstractResult to share some code.

---
+++ src/AbstractResult.php
@@ -0,0 +1,186 @@
+<?php
+namespace WScore\Validation;
+
+use WScore\Validation\Interfaces\ResultInterface;
+use WScore\Validation\Locale\Messages;
+
+abstract class AbstractResult implements ResultInterface
+{
+    /**
+     * @var null|array
+     */
+    private $value = [];
+
+    /**
+     * @var null|array
+     */
+    private $originalValue = [];
+
+    /**
+     * @var bool
+     */
+    protected $isValid = true;
+
+    /**
+     * @var Messages
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $name = '';
+
+    /**
+     * @var Result[]
+     */
+    protected $children = [];
+
+    /**
+     * @var ResultList
+     */
+    private $parent;
+
+    /**
+     * @var array
+     */
+    private $failed = [];
+
+    /**
+     * Result constructor.
+     * @param Messages|null $message
+     * @param string $value
+     * @param null|string $name
+     */
+    public function __construct(?Messages $message, $value, $name = null)
+    {
+        $this->message = $message;
+        $this->value = $this->originalValue = $value;
+        $this->name = $name;
+    }
+
+    /**
+     * @param string $failedAt
+     * @param array $options
+     * @param string $message
+     * @return $this
+     */
+    public function failed(string $failedAt, array $options = [], string $message = null): ResultInterface
+    {
+        if ($message === null && $this->message) {
+            $message = $this->message->getMessage($failedAt, $options);
+        }
+        $this->failed[] = [
+            'failedAt' => $failedAt,
+            'options' => $options,
+            'message' => $message
+        ];
+        $this->isValid = false;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function name(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|string[]|mixed
+     */
+    public function value()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return string|string[]|array
+     */
+    public function getOriginalValue()
+    {
+        return $this->originalValue;
+    }
+
+    /**
+     * @param mixed $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasChild(string $name): bool
+    {
+        return isset($this->children[$name]);
+    }
+
+    /**
+     * @param string $name
+     * @return ResultInterface
+     */
+    public function getChild(string $name): ?ResultInterface
+    {
+        return $this->children[$name] ?? null;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getErrorMessage()
+    {
+        if ($this->isValid()) {
+            return [];
+        }
+        $messages = [];
+        foreach ($this->failed as $item) {
+            $messages[] = $item['message'];
+        }
+        return $messages;
+    }
+
+    /**
+     * @return self[]|\iterable
+     */
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->getChildren());
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasChildren(): bool
+    {
+        return count($this->children);
+    }
+
+    /**
+     * @return ResultInterface[]
+     */
+    public function getChildren()
+    {
+        return $this->children;
+    }
+
+    /**
+     * @return ResultList|null
+     */
+    public function getParent(): ?ResultList
+    {
+        return $this->parent;
+    }
+
+    /**
+     * @param ResultList $parent
+     */
+    public function setParent(ResultList $parent): void
+    {
+        $this->parent = $parent;
+    }
+}
--- src/ResultList.php
+++ src/ResultList.php
@@ -2,63 +2,9 @@
 namespace WScore\Validation;
 
 use WScore\Validation\Interfaces\ResultInterface;
-use WScore\Validation\Locale\Messages;
 
-class ResultList implements ResultInterface
+class ResultList extends AbstractResult
 {
-    /**
-     * @var null|array
-     */
-    private $value = [];
-
-    /**
-     * @var null|array
-     */
-    private $originalValue = [];
-
-    /**
-     * @var bool
-     */
-    private $isValid = true;
-
-    /**
-     * @var Messages
-     */
-    private $message;
-
-    /**
-     * @var string
-     */
-    private $name = '';
-
-    /**
-     * @var Result[]
-     */
-    private $children = [];
-
-    /**
-     * @var ResultList
-     */
-    private $parent;
-
-    /**
-     * @var array
-     */
-    private $failed = [];
-
-    /**
-     * Result constructor.
-     * @param Messages|null $message
-     * @param string $value
-     * @param null|string $name
-     */
-    public function __construct(?Messages $message, $value, $name = null)
-    {
-        $this->message = $message;
-        $this->value = $this->originalValue = $value;
-        $this->name = $name;
-    }
-
     /**
      * @param ResultInterface $result
      * @param null $name
@@ -68,58 +14,6 @@ class ResultList implements ResultInterface
         $result->setParent($this);
         $name = $name ?? $result->name();
         $this->children[$name] = $result;
-    }
-
-    /**
-     * @param string $failedAt
-     * @param array $options
-     * @param string $message
-     * @return $this
-     */
-    public function failed(string $failedAt, array $options = [], string $message = null): ResultInterface
-    {
-        if ($message === null && $this->message) {
-            $message = $this->message->getMessage($failedAt, $options);
-        }
-        $this->failed[] = [
-            'failedAt' => $failedAt,
-            'options' => $options,
-            'message' => $message
-        ];
-        $this->isValid = false;
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function name(): ?string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @return string|string[]|mixed
-     */
-    public function value()
-    {
-        return $this->value;
-    }
-
-    /**
-     * @return string|string[]|array
-     */
-    public function getOriginalValue()
-    {
-        return $this->originalValue;
-    }
-
-    /**
-     * @param mixed $value
-     */
-    public function setValue($value)
-    {
-        $this->value = $value;
     }
 
     /**
@@ -137,85 +31,12 @@ class ResultList implements ResultInterface
     }
 
     /**
-     * @param string $name
-     * @return bool
-     */
-    public function hasChild(string $name): bool
-    {
-        return isset($this->children[$name]);
-    }
-
-    /**
-     * @param string $name
-     * @return ResultInterface
-     */
-    public function getChild(string $name): ?ResultInterface
-    {
-        return $this->children[$name] ?? null;
-    }
-
-    /**
-     * @return string[]
-     */
-    public function getErrorMessage()
-    {
-        if ($this->isValid()) {
-            return [];
-        }
-        $messages = [];
-        foreach ($this->failed as $item) {
-            $messages[] = $item['message'];
-        }
-        return $messages;
-    }
-
-    /**
-     * @return self[]|\iterable
-     */
-    public function getIterator()
-    {
-        return new \ArrayIterator($this->getChildren());
-    }
-
-    /**
-     * @return bool
-     */
-    public function hasChildren(): bool
-    {
-        return count($this->children);
-    }
-
-    /**
-     * @return ResultInterface[]
-     */
-    public function getChildren()
-    {
-        return $this->children;
-    }
-
-    /**
-     * @return ResultList|null
-     */
-    public function getParent(): ?ResultList
-    {
-        return $this->parent;
-    }
-
-    /**
-     * @param ResultList $parent
-     */
-    public function setParent(ResultList $parent): void
-    {
-        $this->parent = $parent;
-    }
-
-    /**
      * @return  void
      */
     public function finalize()
     {
         $this->summarizeChildren('finalize');
-        $this->value = $this->summarizeChildren('value');
+        $this->setValue($this->summarizeChildren('value'));
         $this->isValid = true;
         foreach ($this->children as $name => $child) {
             if (!$child->isValid()) {
