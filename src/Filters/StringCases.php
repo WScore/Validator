e199655b71833751117c9b84ebcca593f87d03e0
asaokamei <asao.kamei@gmail.com>
2019-04-11 21:23:56 +0900

    added StringCases and StringTrim filters.

---
+++ src/Filters/StringCases.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace WScore\Validation\Filters;
+
+use InvalidArgumentException;
+use WScore\Validation\Interfaces\FilterInterface;
+use WScore\Validation\Interfaces\ResultInterface;
+
+class StringCases extends AbstractFilter
+{
+    const TO_UPPER = 'upper';
+    const TO_LOWER = 'lower';
+    const UC_WORDS = 'uc_wordS';
+    const UC_FIRST = 'uc_first';
+
+    /**
+     * @var string[]
+     */
+    private $functions = [
+        self::TO_UPPER => 'strtoupper',
+        self::TO_LOWER => 'strtolower',
+        self::UC_WORDS => 'ucwords',
+        self::UC_FIRST => 'ucfirst',
+    ];
+
+    /**
+     * @var string[]
+     */
+    private $options;
+
+    /**
+     * list functions to operate in order, as;
+     * [self::TO_LOWER, self::UC_WORDS]
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $this->options = $options;
+        $this->setPriority(FilterInterface::PRIORITY_STRING_FILTERS);
+    }
+
+    /**
+     * @param ResultInterface $input
+     * @return ResultInterface|null
+     */
+    public function __invoke(ResultInterface $input): ?ResultInterface
+    {
+        $value = $input->value();
+        foreach ($this->options as $option) {
+            if (!array_key_exists($option, $this->functions)) {
+                throw new InvalidArgumentException('cannot find function for :' . $option);
+            }
+            $function = $this->functions[$option];
+            $value = call_user_func($function, $value);
+        }
+        $input->setValue($value);
+
+        return null;
+    }
+}
---
+++ src/Filters/StringTrim.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace WScore\Validation\Filters;
+
+use InvalidArgumentException;
+use WScore\Validation\Interfaces\FilterInterface;
+use WScore\Validation\Interfaces\ResultInterface;
+
+class StringTrim extends AbstractFilter
+{
+    const TRIM = 'trim';
+    const LEFT_TRIM = 'ltrim';
+    const RIGHT_TRIM = 'rtrim';
+
+    private $functions = [
+        self::TRIM, self::LEFT_TRIM, self::RIGHT_TRIM,
+    ];
+
+    /**
+     * @var string
+     */
+    private $trim;
+
+    /**
+     * @var string|null
+     */
+    private $mask;
+
+    /**
+     * list functions to operate in order, as;
+     * [self::TO_LOWER, self::UC_WORDS]
+     *
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $this->trim = $options['trim'] ?? self::TRIM;
+        $this->mask = $options['mask'] ?? null;
+        $this->setPriority(FilterInterface::PRIORITY_STRING_FILTERS);
+        if (!in_array($this->trim, $this->functions)) {
+            throw new InvalidArgumentException('No such trim function: ' . $this->trim);
+        }
+    }
+
+    /**
+     * @param ResultInterface $input
+     * @return ResultInterface|null
+     */
+    public function __invoke(ResultInterface $input): ?ResultInterface
+    {
+        $value = $input->value();
+        $function = $this->trim;
+        if ($this->mask !== null) {
+            $value = $function($value, $this->mask);
+        } else {
+            $value = $function($value);
+        }
+        $input->setValue($value);
+
+        return null;
+    }
+}
