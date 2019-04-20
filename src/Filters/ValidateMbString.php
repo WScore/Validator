bec9791c674bbadacdeb693fbd0e9d77e2116de4
asaokamei <asao.kamei@gmail.com>
2019-04-20 09:36:56 +0900

    renamed FilterMbString to ValidateMbString, and validate the input is valid UTF-8 string; now replace ValidateUtf8String when possible.

--- src/Filters/ValidateMbString.php
+++ src/Filters/ValidateMbString.php
@@ -6,17 +6,21 @@ namespace WScore\Validation\Filters;
 use WScore\Validation\Interfaces\FilterInterface;
 use WScore\Validation\Interfaces\ResultInterface;
 
-/**
- * todo: this is converter, but should run at the beginning of the validation loop.
- */
-final class FilterMbString extends AbstractFilter
+final class ValidateMbString extends AbstractFilter
 {
+    use ValidateUtf8Trait;
+
     const MB_HANKAKU = 'aKVs';
     const MB_ZENKAKU = 'AKVS';
     const MB_HAN_KANA = 'khs';
     const MB_ZEN_KANA = 'KV';
     const MB_HIRAGANA = 'HVcS';
     const MB_KATAKANA = 'KVCS';
+
+    /**
+     * @var int
+     */
+    private $max;
 
     /**
      * @var string
@@ -29,6 +33,7 @@ final class FilterMbString extends AbstractFilter
     public function __construct(array $options = [])
     {
         $this->convertType = $options['type'] ?? self::MB_ZEN_KANA;
+        $this->max = $options['max'] ?? 1028*1028; // 1MB
         $this->setPriority(FilterInterface::PRIORITY_FILTER_PREPARE);
     }
 
@@ -39,6 +44,9 @@ final class FilterMbString extends AbstractFilter
     public function apply(ResultInterface $input): ?ResultInterface
     {
         $value = $input->value();
+        if ($bad = $this->checkUtf8($input, $this->max)) {
+            return $bad;
+        }
         $value = mb_convert_kana($value, $this->convertType, 'UTF-8');
         $input->setValue($value);
 
--- src/Locale/ja/validation.types.php
+++ src/Locale/ja/validation.types.php
@@ -6,28 +6,26 @@
  */
 return [
     'text' => [
-        \WScore\Validation\Filters\FilterMbString::class => ['type' => \WScore\Validation\Filters\FilterMbString::MB_ZENKAKU],
-        \WScore\Validation\Filters\ValidateUtf8String::class,
+        \WScore\Validation\Filters\ValidateMbString::class => ['type' => \WScore\Validation\Filters\ValidateMbString::MB_ZENKAKU],
         \WScore\Validation\Filters\DefaultValue::class => ['default' => ''],
     ],
     'integer' => [
-        \WScore\Validation\Filters\FilterMbString::class => ['type' => \WScore\Validation\Filters\FilterMbString::MB_HANKAKU],
+        \WScore\Validation\Filters\ValidateMbString::class => ['type' => \WScore\Validation\Filters\ValidateMbString::MB_HANKAKU],
         \WScore\Validation\Filters\ValidateInteger::class,
         \WScore\Validation\Filters\DefaultValue::class => ['default' => null],
     ],
     'float' => [
-        \WScore\Validation\Filters\FilterMbString::class => ['type' => \WScore\Validation\Filters\FilterMbString::MB_HANKAKU],
+        \WScore\Validation\Filters\ValidateMbString::class => ['type' => \WScore\Validation\Filters\ValidateMbString::MB_HANKAKU],
         \WScore\Validation\Filters\ValidateFloat::class,
         \WScore\Validation\Filters\DefaultValue::class => ['default' => null],
     ],
     'date' => [
-        \WScore\Validation\Filters\FilterMbString::class => ['type' => \WScore\Validation\Filters\FilterMbString::MB_HANKAKU],
+        \WScore\Validation\Filters\ValidateMbString::class => ['type' => \WScore\Validation\Filters\ValidateMbString::MB_HANKAKU],
         \WScore\Validation\Filters\ValidateDateTime::class,
         \WScore\Validation\Filters\DefaultValue::class => ['default' => null],
     ],
     'email' => [
-        \WScore\Validation\Filters\FilterMbString::class => ['type' => \WScore\Validation\Filters\FilterMbString::MB_HANKAKU],
-        \WScore\Validation\Filters\ValidateUtf8String::class,
+        \WScore\Validation\Filters\ValidateMbString::class => ['type' => \WScore\Validation\Filters\ValidateMbString::MB_HANKAKU],
         \WScore\Validation\Filters\DefaultValue::class => ['default' => ''],
         \WScore\Validation\Filters\Match::class => ['type' => \WScore\Validation\Filters\Match::EMAIL],
     ],
