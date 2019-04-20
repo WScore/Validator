4c3dd3414bbd56fd8ea73acbbae7bb538078fd0c
asaokamei <asao.kamei@gmail.com>
2019-04-20 11:19:22 +0900

    add case for post: one-to-many form test.

--- tests/RealCaseTest.php
+++ tests/RealCaseTest.php
@@ -75,6 +75,18 @@ class RealCaseTest extends TestCase
                 ],
             ]));
 
+        $posts = $vb->form()
+            ->add('title', $vb->text([
+                Required::class,
+            ]))
+            ->add('publishedAt', $vb->date([
+            ]))
+            ->add('size', $vb->integer([
+                Required::class,
+            ]));
+
+        $form->addRepeatedForm('posts', $posts);
+
         $form->add('address', $address);
 
         return $form;
@@ -106,5 +118,19 @@ class RealCaseTest extends TestCase
         $this->assertEquals('12345', $address->getChild('zip')->value());
         $this->assertEquals('city, street 101', $address->getChild('address')->value());
         $this->assertEquals('ABC Country', $address->getChild('region')->value());
+
+        /* check posts input. */
+        $this->assertTrue($results->hasChild('posts'));
+        $posts = $results->getChild('posts');
+        // check post0.
+        $post0 = $posts->getChild(0);
+        $this->assertEquals('first title', $post0->getChild('title')->value());
+        $this->assertEquals(null, $post0->getChild('publishedAt')->value());
+        $this->assertEquals(1234, $post0->getChild('size')->value());
+        // check post1.
+        $post1 = $posts->getChild(1);
+        $this->assertEquals('more tests here', $post1->getChild('title')->value());
+        $this->assertEquals('20190401', $post1->getChild('publishedAt')->value()->format('Ymd'));
+        $this->assertEquals(2345, $post1->getChild('size')->value());
     }
 }
