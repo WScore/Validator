17ac2c06c6725d6d76a7eacbe8892e72d7aed639
亀井亜佐夫 <asao.kamei@gmail.com>
2019-05-31 16:58:57 +0900

    added a filter to remove inputs that are null, empty string, or empty array.

---
+++ src/Filters/FilterEmptyValues.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace WScore\Validation\Filters;
+
+use InvalidArgumentException;
+use WScore\Validation\Interfaces\FilterInterface;
+use WScore\Validation\Interfaces\ResultInterface;
+
+/**
+ * removes any inputs that are null or empty string, or empty array.
+ * use this filter for one-to-many forms to ignore empty rows.
+ */
+final class FilterEmptyValues extends AbstractFilter
+{
+
+    /**
+     * @param ResultInterface $input
+     * @return ResultInterface|null
+     */
+    public function apply(ResultInterface $input): ?ResultInterface
+    {
+        $values = $input->value();
+        if ($this->isEmpty($values)) {
+            $input->setValue(null);
+        }
+        if (!is_array($values)) {
+            return null;
+        }
+        $values = $this->cleanUp($values);
+        $input->setValue($values);
+
+        return null;
+    }
+
+    private function cleanUp(array $values)
+    {
+        foreach ($values as $key => $value) {
+            if (is_array($value)) {
+                $value = $this->cleanUp($value);
+            }
+            if ($this->isEmpty($value)) {
+                unset($values[$key]);
+            } else {
+                $values[$key] = $value;
+            }
+        }
+        return $values;
+    }
+}
